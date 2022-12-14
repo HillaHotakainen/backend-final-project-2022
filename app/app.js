6b92f3b4cde0d60edc7bea0dca7798f246068820
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-12-14 18:02:05 +0200

    trying to fix CORS error

--- app/app.js
+++ app/app.js
@@ -6,6 +6,10 @@ const expensesRouter = require("./routes/expenses");
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use("/api/expenses", expensesRouter);
 
 module.exports = app;
