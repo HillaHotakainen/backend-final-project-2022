bf97744a13bce637bf5f0a7cb24fd189d1d231f2
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-12-15 13:01:32 +0200

    fixing CORS

--- app/app.js
+++ app/app.js
@@ -1,15 +1,17 @@
 const { request } = require("express");
 const express = require("express");
+const cors = require("cors");
 
 const expensesRouter = require("./routes/expenses");
 
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
 app.use("/api/expenses", expensesRouter);
+app.use(
+  cors({
+    origin: ["http://localhost:5000", "https://project-igip.onrender.com/"],
+  })
+);
 
 module.exports = app;
