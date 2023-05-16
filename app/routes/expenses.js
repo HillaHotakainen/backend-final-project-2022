0f901fea2511da3e810317be5dc2be440d69c157
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2023-05-16 15:01:15 +0300

    overwrite wrong file

---
+++ app/routes/expenses.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const {
+  getExpenses,
+  getExpensesByMonth,
+  getExpensesByDate,
+  getExpensesByCost,
+  getExpensesByShop,
+  getExpensesByCategory,
+  createExpense,
+  deleteExpense,
+  updateExpense,
+} = require("../controllers/expenses");
+
+const router = express.Router();
+
+router.get("/", getExpenses);
+router.get("/:month", getExpensesByMonth);
+router.get("/date/:date", getExpensesByDate);
+router.get("/cost/:amount", getExpensesByCost);
+router.get("/shop/:shop", getExpensesByShop);
+router.get("/category/:category", getExpensesByCategory);
+router.post("/", createExpense);
+router.delete("/:id", deleteExpense);
+router.put("/", updateExpense);
+
+module.exports = router;
